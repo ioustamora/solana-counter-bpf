9e2d47354115d5c06f2082f783905faa90b541e7
ioustamora <ioustamora@protonmail.com>
2021-10-29 00:45:07 +0300

    step

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
 pub mod state;
 pub mod instructions;
+pub mod processor;
+pub mod error;
+
+#[cfg(not(feature = "no-entrypoint"))]
 pub mod entrypoint;
+
+pub const COUNTER_SEED: &str = "counter";
+pub const SETTINGS_SEED: &str = "settings";
+
+solana_program::declare_id!("9onZvMzqAFzSHJrLNVWfqLRFFQ5ZCGzNXB4PBxmp6z5Y");
--- src/state.rs
+++ src/state.rs
@@ -3,13 +3,35 @@ use solana_program::pubkey::Pubkey;
 
 use crate::{id, COUNTER_SEED, SETTINGS_SEED};
 
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct Counter {
     pub counter: u32,
     pub value: i64,
-};
+}
+
+impl Counter {
+    pub fn get_counter_pubkey(user: &Pubkey) -> Pubkey {
+        Pubkey::create_with_seed(user, COUNTER_SEED, &id()).unwrap()
+    }
+
+    pub fn is_ok_counter_pubkey(user: &Pubkey, counter: &Pubkey) -> bool {
+        counter.to_bytes() == Self::get_counter_pubkey(user).to_bytes()
+    }
+}
 
 pub struct Settings {
     owner: [u8, 32],
     inc_step: u32,
     dec_step: u32,
-};
+}
+
+impl Settings {
+    pub fn get_settings_pubkey_with_bump() -> (Pubkey, u8) {
+        Pubkey::find_program_address(&[SETTINGS_SEED.as_bytes()], &id())
+    }
+
+    pub fn get_settings_pubkey() -> Pubkey {
+        let (pubkey, _) = Self::get_settings_pubkey_with_bump();
+        pubkey
+    }
+}
